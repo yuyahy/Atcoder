fc3d1ad0494635d9aab7020f7b9e3bc9957b02a3
yuyahy <xvto.ya@gmail.com>
2023-10-14 01:49:44 +0000

    鉄則の実装(A11、B11)

---
+++ tessoku-book/A11/main.cpp
@@ -0,0 +1,43 @@
+// デバッグ用関数の設定
+#ifdef DEFINED_ONLY_IN_LOCAL
+#include </workspaces/atcoder/cpp-dump/dump.hpp>
+#define dump(...) CPP_DUMP(__VA_ARGS__)
+#else
+#define dump(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#endif
+
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+// mint
+using mint = static_modint<1000000007>;
+// ll int
+ll INF = numeric_limits<ll>::max() / 2;
+
+int main() {
+  // set precision (10 digit)
+  cout << setprecision(10);
+
+  int N(0), X(0);
+  cin >> N >> X;
+  vector<int> numbers(N, 0);
+  int tmp = 0;
+  for (int i = 0; i < N; i++) {
+    cin >> tmp;
+    numbers[i] = tmp;
+  }
+
+  // ソート(昇順)
+  sort(numbers.begin(), numbers.end());
+  auto position =
+      lower_bound(numbers.begin(), numbers.end(), X);  // X を二分探索
+  auto idx_lower = distance(numbers.begin(), position);
+  cout << idx_lower + 1 << endl;
+}
