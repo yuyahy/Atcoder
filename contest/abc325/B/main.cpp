31a7c697287dde1efb795be8308b245f3686255b
yuyahy <xvto.ya@gmail.com>
2023-10-22 09:04:42 +0000

    ABC325(B問題の別解答試みたが、上手くいかず)

--- contest/abc325/B/main.cpp
+++ contest/abc325/B/main.cpp
@@ -33,23 +33,49 @@ int main() {
     cin >> W[i] >> X[i];
   }
   ll max = 0;
-  for (int i = 0; i < 24; i++) {
-    ll attendence = 0;
-    for (int j = 0; j < N; j++) {
-      int local_time = 0;
-      if (X[j] + i <= 23) {
-        local_time = X[j] + i;
-      } else {
-        local_time = X[j] + i - 24;
+  bool bMyAnswer(false);
+  if (bMyAnswer) {
+    for (int i = 0; i < 24; i++) {
+      ll attendence = 0;
+      for (int j = 0; j < N; j++) {
+        int local_time = 0;
+        // Note: 以下は自分の提出時の実装
+        // →解答例にある様に、24の余りを利用すると場合分けせずに済む
+        // if (X[j] + i <= 23) {
+        //   local_time = X[j] + i;
+        // } else {
+        //   local_time = X[j] + i - 24;
+        // }
+        local_time = (X[j] + i) % 24;
+        if (local_time >= 9 && local_time + 1 <= 18) {
+          attendence += W[j];
+        }
       }
-      if (local_time >= 9 && local_time + 1 <= 18) {
-        attendence += W[j];
-        // dump(local_time);
-        // dump(i);
-        // dump(X[j]);
-      }
+      max = std::max(max, attendence);
     }
-    max = std::max(max, attendence);
+  } else {
+    // いもす法
+    // Note: こちら試してみたが、うまくいかなかったので一旦放置する
+    // →おそらく時差で日をまたぐパターンの考慮が上手くできていない？
+    vector<ll> attendence(24, 0);
+    for (int i = 0; i < N; i++) {
+      auto begin_time = (9 - X[i]) >= 0 ? (9 - X[i]) : 0;
+      auto end_time = (18 - X[i]) >= 0 ? (18 - X[i]) : 23;
+      if (begin_time > end_time) std::swap(begin_time, end_time);
+      attendence[begin_time] += W[i];
+      attendence[end_time] -= W[i];
+    }
+    dump(attendence);
+    // 累積和
+    auto accum_attendence = attendence;
+    for (int i = 0; i < N; i++) {
+      if (i != 0) {
+        accum_attendence[i] += accum_attendence[i - 1];
+      }
+      max = std::max(max, accum_attendence[i]);
+    }
+    dump(accum_attendence);
   }
+
   cout << max << endl;
 }
