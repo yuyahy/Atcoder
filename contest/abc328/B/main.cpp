7dfd1cf10ccce7f81c7777dd06a70a99450b7d11
yuyahy <xvto.ya@gmail.com>
2023-11-12 09:29:29 +0000

    ABC328(解説読んで振り返り)

--- contest/abc328/B/main.cpp
+++ contest/abc328/B/main.cpp
@@ -22,21 +22,32 @@ using mint = static_modint<1000000007>;
 // ll int
 ll INF = numeric_limits<ll>::max() / 2;
 
-int double_num(int num) {
+// 入力された数値がゾロ目(1もOK)かを判定する
+bool IsSameNumbers(int num) {
   int y;
 
   /* １桁の場合、ゾロ目ではないので終了 */
-  if (!(num / 10)) return 1;
+  if (!(num / 10)) return true;
 
   y = num % 10; /* １の位を記憶 */
-  while (1) {
+  while (true) {
     num /= 10; /* 10で割って桁を下げる。1234なら123になる。 */
     if (!num) break; /* 位が無くなると終了 */
 
     if (y != num % 10) /* １の位とXの位を比較。 */
-      return 0;        /* 違っていたら終了 */
+      return false;    /* 違っていたら終了 */
   }
-  return 1;
+  return true;
+}
+
+// 入力された文字列が1種類の文字列で構成されているかを判定する
+bool IsSameStrings(string str) {
+  set<char> str_set;
+  // for (int i = 0; i < str.length(); i++) {
+  //   str_set.insert(str[i]);
+  // }
+  // return str_set.size() == 1 ? true : false;
+  return (size(set<char>(str.begin(), str.end())) == 1);
 }
 
 int main() {
@@ -49,31 +60,38 @@ int main() {
     cin >> D[i];
   }
 
+  // 計算量がO(N*Di)で、条件より高々100^2 = 10000程度なので日付を全探索する
+  // →月と日にちの内、それぞれゾロ目であるかをチェックし、ゾロ目の場合は月を構成する1種類の数字と日にちの1種類の数字が一致しているかチェックする
+  // 他の解法としては、設問の条件から、ゾロ目の日付としてあり得る物を全列挙しておき(今回は36通り)、その内入力値の日付の範囲内に含まれる物が何個あるかをカウントする方法がある
+  //
+  // ある数値がゾロ目かどうかは以下の2種類の方法でチェックできる
+  // 1.
+  // 10で割れるだけ割り、その余りを見て1桁目と全て一致していればゾロ目(1桁の数値はすべてゾロ目として扱う)
+  // 2.
+  // 月と日にちを文字列に変換 &
+  // 結合し、それが1種類の文字列で構成されているかチェックする(std::setの長さが1ならゾロ目)
   int sum(0);
+  bool bUseIsSameNumbers(false);
   for (int i = 1; i <= N; i++) {
-    if (double_num(i) == 0) continue;
-    string str1 = to_string(i);
-    for (int j = 1; j <= D[i]; j++) {
-      if (double_num(j) == 0) continue;
-      string str = to_string(j);
-      bool bMatch(true);
-      for (int k = 0; k < str.length(); k++) {
-        if (str[k] != str1[0]) bMatch = false;
+    if (bUseIsSameNumbers) {
+      if (!IsSameNumbers(i)) continue;
+      string str1 = to_string(i);
+      for (int j = 1; j <= D[i]; j++) {
+        if (!IsSameNumbers(j)) continue;
+        string str2 = to_string(j);
+        // ここに到達している時点で月と日にちがゾロ目であることは確定しているので、
+        // 適当な桁から一文字だけ抽出して同じか比較する
+        bool bMatch = str1[0] == str2[0] ? true : false;
+        if (bMatch) sum++;
       }
-      if (bMatch) sum++;
+    } else {
+      string str1 = to_string(i);
+      for (int j = 1; j <= D[i]; j++) {
+        string str2 = to_string(j);
+        auto month_day = str1 + str2;
+        if (IsSameStrings(month_day)) sum++;
+      }
     }
-
-    // int tmp(i), pw(1), origin(i);
-    // while (true) {
-    //   if (tmp <= D[i]) {
-    //     tmp += origin * std::pow(10, pw);
-    //     sum++;
-    //     pw++;
-    //     dump(tmp);
-    //   } else {
-    //     break;
-    //   }
-    // }
   }
   cout << sum << endl;
 }
--- contest/abc328/D/main.cpp
+++ contest/abc328/D/main.cpp
@@ -31,21 +31,44 @@ int main() {
   // 先頭からi文字目までの文字列から"ABC"を削除した文字列
   vector<string> DP(str.length(), "");
   DP[0] = str;
-  for (int i = 1; i < str.length(); i++) {
-    // auto sub = str.substr(i);
-    // if (sub.length() < 3) {
-    //   DP[i] = DP[i - 1];
-    // } else {
-    auto pos = DP[i - 1].find("ABC");
-    if (pos == string::npos) {
-      DP[i] = DP[i - 1];
-    } else {
-      DP[i] = DP[i - 1].erase(pos, 3);
+  // Note:
+  // 入力された文字列を先頭から探索→"ABC"が見つかったら削除という方針だと、
+  // 探索にO(N)、"ABC"を削除した際に、std::string::erase()の内部で文字列を並べ直す処理にO(N)かかり、
+  // 最悪O(N^2)かかり間に合わない。
+  // よって、std::stackなどを用いてO(N)に計算回数を落とす工夫が必要になる
+  // →後ろから"ABC"が追加された時点で都度削除すれば、それより前の要素を並べ直す時間はかからないので削除の仕方がポイント。
+  std::stack<char> str_stack;
+  dump(str_stack);
+  // 先頭から探索すると、std::stackの性質上(先入れ先出し)、逆順で出力しないと文字列の順序が逆転してしまうため、
+  // 逆順で探索する
+  // Note: str.length()始まりにしてしまうと、エラーにならない &
+  // 謎の空要素がpushされてしまうので注意
+  for (int i = str.length() - 1; i >= 0; i--) {
+    str_stack.push(str[i]);
+    // 要素の追加ごとに"ABC"が含まれていないかチェックし、含まれている場合は削除する
+    if (str_stack.size() >= 3 && str[i] == 'A') {
+      // 先頭
+      auto tmp_i = str_stack.top();
+      str_stack.pop();
+      // 2番目
+      auto tmp_i_1 = str_stack.top();
+      str_stack.pop();
+      // 3番目
+      auto tmp_i_2 = str_stack.top();
+      str_stack.pop();
+      // 先頭が"A"である事はチェック済みなので、残りの"BC"のみチェックする
+      // →もし"ABC"になっていなければ詰め直す必要がある
+      if (tmp_i_1 != 'B' || tmp_i_2 != 'C') {
+        str_stack.push(tmp_i_2);
+        str_stack.push(tmp_i_1);
+        str_stack.push(tmp_i);
+      }
     }
-    // }
   }
   dump(DP);
+  dump(str_stack);
 
+  // 以下は最初に書いた方針だが、TLEした
   //   while (true) {
   //     auto pos = str.find("ABC");
   //     if (pos == string::npos) {
@@ -54,5 +77,9 @@ int main() {
   //       str = str.erase(pos, 3);
   //     }
   //   }
-  cout << DP[str.length() - 1] << endl;
+  while (!str_stack.empty()) {
+    cout << str_stack.top();
+    str_stack.pop();
+  }
+  cout << endl;
 }
