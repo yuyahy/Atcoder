10236840df277598815263f684c3ab3897958e4a
yuyahy <xvto.ya@gmail.com>
2023-12-14 12:27:35 +0000

    鉄則の実装(A51、B51、ヒューリスティックは一旦スキップ)

---
+++ tessoku-book/A51/main.cpp
@@ -0,0 +1,49 @@
+// デバッグ用関数の設定
+#ifdef DEFINED_ONLY_IN_LOCAL
+#include </workspaces/atcoder/cpp-dump/dump.hpp>
+#define dump(...) CPP_DUMP(__VA_ARGS__)
+#else
+#define dump(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#endif
+
+#define REP(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define REP_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define REP3R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+// mint
+using mint = static_modint<1000000007>;
+// ll int
+ll INF = numeric_limits<ll>::max() / 2;
+
+int main() {
+  // set precision (10 digit)
+  cout << setprecision(10);
+  int Q(0);
+  cin >> Q;
+  stack<string> books;
+
+  REP(i, Q) {
+    auto query(0);
+    cin >> query;
+    if (query == 1) {
+      string str("");
+      cin >> str;
+      books.push(str);
+    } else if (query == 2) {
+      cout << books.top() << endl;
+    } else {
+      books.pop();
+    }
+  }
+}
---
+++ tessoku-book/B51/main.cpp
@@ -0,0 +1,45 @@
+// デバッグ用関数の設定
+#ifdef DEFINED_ONLY_IN_LOCAL
+#include </workspaces/atcoder/cpp-dump/dump.hpp>
+#define dump(...) CPP_DUMP(__VA_ARGS__)
+#else
+#define dump(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#endif
+
+#define REP(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define REP_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define REP3R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace atcoder;
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+// mint
+using mint = static_modint<1000000007>;
+// ll int
+ll INF = numeric_limits<ll>::max() / 2;
+
+int main() {
+  // set precision (10 digit)
+  cout << setprecision(10);
+
+  stack<int> indices;
+  string brackets("");
+  cin >> brackets;
+
+  REP(i, brackets.length()) {
+    if (brackets[i] == '(') {
+      indices.push(i);
+    } else {
+      cout << indices.top() + 1 << " " << i + 1 << endl;
+      indices.pop();
+    }
+  }
+}
