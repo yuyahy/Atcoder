ecfd3f2a0f3e1d6a5f01ea6c9a6aae748a9d1eba
yuyahy <xvto.ya@gmail.com>
2023-12-14 12:43:47 +0000

    鉄則の実装(A52)

---
+++ tessoku-book/A52/main.cpp
@@ -0,0 +1,69 @@
+// TODO: マクロや共通関数は別ファイルに定義し、埋め込む様にする
+// デバッグ用関数の設定
+#ifdef DEFINED_ONLY_IN_LOCAL
+#include </workspaces/atcoder/cpp-dump/dump.hpp>
+#define dump(...) CPP_DUMP(__VA_ARGS__)
+#else
+#define dump(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#endif
+
+#define REP(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define REP_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define REP3R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+// nCk、組み合わせを求める関数
+//
+// usage:
+// vector<vector<ll> > v(n + 1, vector<ll>(n + 1, 0));
+// comb(v);
+// v[n][k]が求めるnCkの値
+void comb(vector<vector<ll> >& v) {
+  for (ll i = 0; i < (ll)v.size(); i++) {
+    v[i][0] = 1;
+    v[i][i] = 1;
+  }
+  for (ll k = 1; k < (ll)v.size(); k++) {
+    for (ll j = 1; j < k; j++) {
+      v[k][j] = (v[k - 1][j - 1] + v[k - 1][j]);
+    }
+  }
+}
+
+// mint
+using mint = static_modint<1000000007>;
+// ll int
+ll INF = numeric_limits<ll>::max() / 2;
+
+int main() {
+  // set precision (10 digit)
+  cout << setprecision(10);
+  int Q(0);
+  cin >> Q;
+  int query(0);
+  queue<string> people;
+
+  REP(i, Q) {
+    cin >> query;
+    if (query == 1) {
+      string name("");
+      cin >> name;
+      people.push(name);
+    } else if (query == 2) {
+      cout << people.front() << endl;
+    } else {
+      people.pop();
+    }
+  }
+}
