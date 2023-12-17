b2cd28e6bf76ad60f9e8522e1eea3c421d9c0802
yuyahy <xvto.ya@gmail.com>
2023-12-17 23:04:40 +0000

    鉄則の実装(優先度付きキューでB39を実装)

--- tessoku-book/B39/main.cpp
+++ tessoku-book/B39/main.cpp
@@ -6,7 +6,10 @@
 #define dump(...)
 #define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
 #endif
-
+#define REP(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define REP_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define REP3R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
 #include <bits/stdc++.h>
 
 #include <atcoder/all>
@@ -38,33 +41,59 @@ int main() {
   ll N(0), D(0), X(0), Y(0);
   cin >> N >> D;
   vector<pair<int, int>> jobs;
+
+  // スコアの高い順に降順で並び替えるqueue
+  priority_queue<int> items;
+  // i日目からできる仕事のmap
+  map<int, vector<int>> job_map;
+
   for (int i = 0; i < N; i++) {
     cin >> X >> Y;
     jobs.push_back({X, Y});
+    job_map[X].push_back(Y);
   }
-  dump(jobs);
-  // スコア順に並べる
-  sort(jobs.begin(), jobs.end(), custom_comp);
-  dump(jobs);
+  dump(items);
 
-  // スコアの高い順に仕事できる場合は仕事する
-  vector<ll> job_list(D + 1, 0);
-  for (int i = 0; i < N; i++) {
-    // その仕事の解禁日が空いていればやる
-    if (job_list[jobs[i].first] == 0) {
-      job_list[jobs[i].first] = jobs[i].second;
-    } else {
-      // jobs[i].first以降の日で空いている日がないかチェック
-      for (int j = jobs[i].first + 1; j < D + 1; j++) {
-        // 空いている日があった場合は、その中の一番最初の日付にその仕事をやる
-        if (job_list[j] == 0) {
-          job_list[j] = jobs[i].second;
-          break;
+  bool bUsePriorityQueue(true);
+  if (bUsePriorityQueue) {
+    int max(0);
+    // i+1日目ごとにできる仕事を優先度付きキューにつめる
+    // Note: 入力値の受け取り時にキューにつめると、一部ケースでTLEする
+    //       要素数が多くなるほど、挿入の度に並び替えるコストが発生するせいかもしれない
+    //       ソートでも挿入の度にやるより、要素を一通り入れてから一回だけソートした方が速いのと同じ？
+    REP(i, D) {
+      for (const auto& job : job_map[i + 1]) {
+        items.push(job);
+      }
+      if (items.empty()) continue;
+      auto salary = items.top();
+      max += salary;
+      items.pop();
+    }
+    cout << max << endl;
+  } else {
+    // スコア順に並べる
+    sort(jobs.begin(), jobs.end(), custom_comp);
+    dump(jobs);
+    // スコアの高い順に仕事できる場合は仕事する
+    vector<ll> job_list(D + 1, 0);
+    for (int i = 0; i < N; i++) {
+      // その仕事の解禁日が空いていればやる
+      if (job_list[jobs[i].first] == 0) {
+        job_list[jobs[i].first] = jobs[i].second;
+      } else {
+        // jobs[i].first以降の日で空いている日がないかチェック
+        for (int j = jobs[i].first + 1; j < D + 1; j++) {
+          // 空いている日があった場合は、その中の一番最初の日付にその仕事をやる
+          if (job_list[j] == 0) {
+            job_list[j] = jobs[i].second;
+            break;
+          }
         }
       }
     }
+    ll sum = std::accumulate(job_list.begin(), job_list.end(), 0);
+    dump(job_list);
+    cout << sum << endl;
   }
-  ll sum = std::accumulate(job_list.begin(), job_list.end(), 0);
-  dump(job_list);
-  cout << sum << endl;
 }
