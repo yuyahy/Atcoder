0b1295d0829b4812725e56c3945369c6e6074731
yuyahy <xvto.ya@gmail.com>
2024-04-20 05:09:05 +0000

    ABC349(A-C問題のリハビリ)

---
+++ contest/abc349/B/main.cpp
@@ -0,0 +1,64 @@
+// TODO: マクロや共通関数は別ファイルに定義し、埋め込む様にする
+// デバッグ用関数の設定
+#ifdef DEFINED_ONLY_IN_LOCAL
+#include </workspaces/atcoder/cpp-dump/dump.hpp>
+#define dump(...) CPP_DUMP(__VA_ARGS__)
+#else
+#define dump(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#endif
+
+#define REP(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define REP_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define REP3R(i, m, n) for (int i = (int)(m)-1; (i) >= (int)(n); --(i))
+
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+// mint
+using mint = static_modint<1000000007>;
+// ll int
+ll INF = numeric_limits<ll>::max() / 2;
+
+// 4近傍(左上原点)
+// Y = (↑, そのまま, ↓, そのまま)
+// X = (そのまま, →, そのまま, ←)
+const int dy_4[4] = {-1, 0, 1, 0};
+const int dx_4[4] = {0, 1, 0, -1};
+// 8近傍
+const int dy_8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dx_8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+int main() {
+    // set precision (10 digit)
+    cout << setprecision(10);
+
+    string S;
+    cin >> S;
+    // <文字, 出現回数>
+    map<char, int> cnt_map;
+    REP(i, S.length()) cnt_map[S[i]]++;
+
+    // <出現回数, 種類>
+    map<int, int> cnt_map_2;
+    for (const auto& elem : cnt_map) {
+        cnt_map_2[elem.second]++;
+    }
+
+    // 各出現回数が0 or 2かチェック
+    bool b_good(true);
+    for (const auto& elem : cnt_map_2)
+        if (elem.second != 0 && elem.second != 2) {
+            b_good = false;
+            break;
+        }
+    cout << (b_good ? "Yes" : "No") << endl;
+}
