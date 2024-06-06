5538613c2bad508dce05d3ccafaa092eabc44cf4
yuyahy <xvto.ya@gmail.com>
2024-06-06 12:52:57 +0000

    精進(灰Diff、ABC215-C)

---
+++ contest/abc215/C/main.cpp
@@ -0,0 +1,112 @@
+// TODO: マクロや共通関数は別ファイルに定義し、埋め込む様にする
+// デバッグ用関数の設定
+#ifdef DEFINED_ONLY_IN_LOCAL
+#include </workspaces/atcoder/cpp-dump/dump.hpp>
+#define dump(...) CPP_DUMP(__VA_ARGS__)
+#else
+#define dump(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#endif
+
+#define REP(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define REP3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define REP_R(i, n) for (int i = (int)(n) - 1; (i) >= 0; --(i))
+#define REP3R(i, m, n) for (int i = (int)(m) - 1; (i) >= (int)(n); --(i))
+
+#define YESNO(T)               \
+    if (T) {                   \
+        cout << "YES" << endl; \
+    } else {                   \
+        cout << "NO" << endl;  \
+    }
+#define yesno(T)               \
+    if (T) {                   \
+        cout << "yes" << endl; \
+    } else {                   \
+        cout << "no" << endl;  \
+    }
+#define YesNo(T)               \
+    if (T) {                   \
+        cout << "Yes" << endl; \
+    } else {                   \
+        cout << "No" << endl;  \
+    }
+
+#define COUT(x) cout << (x) << endl
+#define SCOUT(x) cout << (x) << " "
+#define ENDL cout << endl
+
+#define ALL(a) (a).begin(), (a).end()
+
+#define INPUT_VEC(a) \
+    for (auto& elem : a) cin >> elem
+
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+
+using namespace atcoder;
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vll;
+
+// mint
+using mint = static_modint<1000000007>;
+// ll int
+ll INF = numeric_limits<ll>::max() / 2;
+
+// 4近傍(左上原点)
+// Y = (↑, そのまま, ↓, そのまま)
+// X = (そのまま, →, そのまま, ←)
+const int dy_4[4] = {-1, 0, 1, 0};
+const int dx_4[4] = {0, 1, 0, -1};
+// 8近傍
+const int dy_8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dx_8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+// アルファベット(小文字大文字)
+const string lower_alphabet = "abcdefghijklmnopqrstuvwxyz";
+const string upper_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// 数字
+const string integers = "0123456789";
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // set precision (10 digit)
+    cout << setprecision(10);
+
+    // 考察メモ
+    // - Smax=8より、8! = 40320なので全列挙可能
+    // - 列挙はstd::next_permutation()で可能？
+
+    // Note:
+    // - 結果: {自力AC}
+    // {自力ACの場合}なぜ解けたか？
+    // - ACに結びついた点
+    // 8!は全探索が十分可能なオーダである事を気づけた
+    // next_permutation()を使う事で順列を簡単に生成する事ができる事を知っていた
+    // - 改善点
+    //  // 特になし
+
+    string S;
+    cin >> S;
+    ll K;
+    cin >> K;
+
+    // next_permutation()を使うために事前にsortしておく必要がある
+    sort(ALL(S));
+
+    ll idx(0);
+    do {
+        dump(S);
+        if (idx == K - 1) {
+            COUT(S);
+            return 0;
+        }
+        idx++;
+    } while (std::next_permutation(ALL(S)));
+}
